f7adc9d2ae723a10f483a4df2209b334f2604938
Pybounce <pybounce@protonmail.com>
2024-05-19 20:13:33 +0100

    adding vert buffer

--- src/window.rs
+++ src/window.rs
@@ -1,10 +1,46 @@
-use wgpu::PresentMode;
+use wgpu::{ util::DeviceExt, PresentMode };
 use winit::{
     event::{ ElementState, Event, KeyEvent, WindowEvent },
     event_loop::{ ControlFlow, EventLoop },
     keyboard::{ KeyCode, PhysicalKey },
     window::{ Window, WindowBuilder },
 };
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+    //For the attributes part you can use the following, but the result returned by the macro is a temporary value, so you'd have to save it as a const
+    //&wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3]
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+];
 
 struct State<'a> {
     surface: wgpu::Surface<'a>,
@@ -13,6 +49,8 @@ struct State<'a> {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>, //todo: why store size here if it's in config.[height/width]?
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
     //More comments stolen from online docs
     // The window must be declared after the surface so
     // it gets dropped after it as the surface contains
@@ -105,7 +143,7 @@ impl<'a> State<'a> {
                 vertex: wgpu::VertexState {
                     module: &shader,
                     entry_point: "vs_main",
-                    buffers: &[],
+                    buffers: &[Vertex::desc()],
                 },
                 fragment: Some(wgpu::FragmentState {
                     module: &shader,
@@ -137,14 +175,26 @@ impl<'a> State<'a> {
             })
         );
 
+        let vertex_buffer = device.create_buffer_init(
+            &(wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            })
+        );
+
+        let num_vertices = VERTICES.len() as u32;
+
         Self {
             surface,
             device,
             queue,
             config,
             size,
-            window,
             render_pipeline,
+            vertex_buffer,
+            num_vertices,
+            window,
         }
     }
 
@@ -207,7 +257,8 @@ impl<'a> State<'a> {
         );
 
         render_pass.set_pipeline(&self.render_pipeline);
-        render_pass.draw(0..3, 0..1);
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+        render_pass.draw(0..self.num_vertices, 0..1);
         drop(render_pass);
 
         // submit will accept anything that implements IntoIter
