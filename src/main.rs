2dd636bbf3e784427a14523f8a24917d0ddf82c4
Pybounce <pybounce@protonmail.com>
2024-05-19 08:56:17 +0100

    what the fuck is happening

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod window;
+pub mod wgpu;
 
 fn main() {
     window::run_window();
--- src/window.rs
+++ src/window.rs
@@ -5,6 +5,7 @@ use winit::{
     keyboard::{ KeyCode, PhysicalKey },
     window::Window,
 };
+use wgpu::PresentMode;
 
 #[derive(Default)]
 struct App {
@@ -58,12 +59,125 @@ impl ApplicationHandler for App {
     }
 }
 
-pub fn run_window() {
+struct State<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    //More comments stolen from online docs
+    // The window must be declared after the surface so
+    // it gets dropped after it as the surface contains
+    // unsafe references to the window's resources.
+    window: &'a Window,
+}
+
+impl<'a> State<'a> {
+    // Creating some of the wgpu types requires async code
+    async fn new(window: &'a Window) -> State<'a> {
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            #[cfg(not(target_arch = "wasm32"))]
+            backends: wgpu::Backends::PRIMARY,
+            #[cfg(target_arch = "wasm32")]
+            backends: wgpu::Backends::GL,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+
+        let adapter = instance
+            .request_adapter(
+                &(wgpu::RequestAdapterOptions {
+                    power_preference: wgpu::PowerPreference::default(),
+                    compatible_surface: Some(&surface),
+                    force_fallback_adapter: false,
+                })
+            ).await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &(wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::empty(),
+                    // WebGL doesn't support all of wgpu's features, so if
+                    // we're building for the web we'll have to disable some.
+                    required_limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                }),
+                // Some(&std::path::Path::new("trace")), // Trace path
+                None
+            ).await
+            .unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code in this tutorial assumes an Srgb surface texture. Using a different
+        // one will result all the colors comming out darker. If you want to support non
+        // Srgb surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps.formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: PresentMode::Fifo,
+            alpha_mode: surface_caps.alpha_modes[0],
+            desired_maximum_frame_latency: 2,
+            view_formats: vec![],
+        };
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window,
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
+
+pub async fn run_window() {
     let event_loop = EventLoop::new().unwrap();
     //Control flow Wait will pauses the event loop until an event is raised
     //Control flow Poll continously runs the event loop, even if there are no events
     //Poll is used for things like games, Wait uses less compute (power/CPU) and won't update all the time.
     event_loop.set_control_flow(ControlFlow::Wait);
+
     let mut app = App::default();
+    let window = &app.window.unwrap();
+    let mut state = State::new(&window).await;
+
     event_loop.run_app(&mut app);
 }
