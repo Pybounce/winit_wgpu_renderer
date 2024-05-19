1fa33cc846895f2bdff60bdb05d4e3602d49f717
Pybounce <pybounce@protonmail.com>
2024-05-19 14:42:34 +0100

    moving to older package versions for compatability

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 pub mod window;
-pub mod wgpu;
 
 fn main() {
-    window::run_window();
+    window::run();
 }
--- src/window.rs
+++ src/window.rs
@@ -1,63 +1,10 @@
 use winit::{
-    application::ApplicationHandler,
-    event::WindowEvent,
-    event_loop::{ ActiveEventLoop, ControlFlow, EventLoop },
+    event::{ ElementState, Event, KeyEvent, WindowEvent },
+    event_loop::{ ControlFlow, EventLoop },
     keyboard::{ KeyCode, PhysicalKey },
-    window::Window,
+    window::{ Window, WindowBuilder },
 };
 use wgpu::PresentMode;
-
-#[derive(Default)]
-struct App {
-    window: Option<Window>,
-}
-
-impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
-    }
-
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent
-    ) {
-        match event {
-            WindowEvent::CloseRequested => {
-                print!("Close requested event");
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                //Below copied from docs
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-                //
-                // Draw.
-                //
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw in
-                // applications which do not always need to. Applications that redraw continuously
-                // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
-            }
-            WindowEvent::KeyboardInput { device_id: _, event, is_synthetic: _ } => {
-                match event.physical_key {
-                    PhysicalKey::Code(KeyCode::Escape) => {
-                        println!("Escape key pressed");
-                        event_loop.exit();
-                    }
-                    _ => (),
-                }
-            }
-            _ => (),
-        }
-    }
-}
 
 struct State<'a> {
     surface: wgpu::Surface<'a>,
@@ -168,16 +115,27 @@ impl<'a> State<'a> {
     }
 }
 
-pub async fn run_window() {
+pub fn run() {
+    env_logger::init();
     let event_loop = EventLoop::new().unwrap();
-    //Control flow Wait will pauses the event loop until an event is raised
-    //Control flow Poll continously runs the event loop, even if there are no events
-    //Poll is used for things like games, Wait uses less compute (power/CPU) and won't update all the time.
-    event_loop.set_control_flow(ControlFlow::Wait);
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    let mut app = App::default();
-    let window = &app.window.unwrap();
-    let mut state = State::new(&window).await;
-
-    event_loop.run_app(&mut app);
+    let _ = event_loop.run(move |event, control_flow| {
+        match event {
+            Event::WindowEvent { ref event, window_id } if window_id == window.id() =>
+                match event {
+                    | WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                          event: KeyEvent {
+                              state: ElementState::Pressed,
+                              physical_key: PhysicalKey::Code(KeyCode::Escape),
+                              ..
+                          },
+                          ..
+                      } => control_flow.exit(),
+                    _ => {}
+                }
+            _ => {}
+        }
+    });
 }
